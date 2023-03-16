0f0b1b6999b44cc27f26691217d998c1a7cffdc2
Benjamin Brouard <bbrouard@akamai.com>
2023-03-16 11:58:10 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,90 @@
+
+<!--- These are examples. See https://shields.io for others or to customize this set of shields. You might want to include dependencies, project status and licence info here --->
+![GitHub repo size](https://img.shields.io/github/repo-size/brrbrr/network-list-toolkit)
+![GitHub contributors](https://img.shields.io/github/contributors/brrbrr/network-list-toolkit)
+![GitHub stars](https://img.shields.io/github/stars/brrbrr/network-list-toolkit?style=social)
+![GitHub forks](https://img.shields.io/github/forks/brrbrr/network-list-toolkit?style=social)
+# Custom-Built Managed Network List Toolkit
+
+This Custom-Built Managed Network List Toolkit allow you the automation of Akamai network lists.
+It perform the following functions:
+
+1. Update existing Network List from source or distant (via URL) CSV file
+2. Activate the Network List on either production or staging networks
+> Note: This toolkit is based on the [Akamai Network List API](https://techdocs.akamai.com/network-lists/reference/api).
+## Introduction
+Akamai Network lists are used to manage collections of IP addresses/CIDR blocks (v4 or v6) or geographic locations.
+The beauty of Network Lists is that you can edit addresses within a network list at any time, without having to edit and reactivate your security policy configuration.
+
+Akamai​ creates and offers packaged network lists for you to use. These lists feature a wave icon beside their name and are usually collections of IP addresses around a common theme, like cloud service networks you may use. These lists are *read-only*, which means you can't edit them, but you can duplicate and edit your own copy.
+
+You can also create your own custom network lists. For example, say you want to allow all your internal IP addresses and those belonging to your vendors. You'd collect them in a network list and activate it. Then in a security policy, you'd use IP/Geo controls to allow all requests from that list. 
+
+But what if my partner is updating his IP addresses on a regular basis ?
+1. You can update manually the network list via the Akamai Control Center
+2. You can use this Custom-Built Managed Network List Toolkit
+
+Thus if your partner is providing you locally or remotely (via URL) the list of IP address in a CSV format file, scheduling to run this script on regular basis (via a CRON job, Jenkins, etc.) will allow you to keep your network-list up to date.
+
+
+> Note: You can include up to 50,000 IP addresses or CIDR blocks per network list and activate up to 32 lists per security configuration. That number includes the following feature-related lists, created automatically: Reputation Allow List, Match Target Bypass List, Rate Controls Bypass List. If you have more than 50,000 IPs, you can split into 2 or more network lists.
+
+## Requirements
+Before you begin, ensure you have met the following requirements:
+
+- Python 3.x
+- akamai-edgegrid (Python client for Akamai APIs)
+- requests
+- argparse
+- csv
+- ipaddress
+
+
+## Usage
+```python
+python3 network-list-toolkit.py list-name [--file] [--delimiter] [--url] [--action] [--network] [--email] [--comment] [--config] [--section] [--accountkey]
+```
+
+## Arguments
+
+- `list-name`: the name of the network list to modify.
+- `--file`: path to CSV file with IPs for the network list. Default is list.csv in current directory.
+- `--delimiter`: CSV delimiter used. Default is ,.
+- `--url`: URL to CSV file with IPs for the network list.
+- `--action`: the action to take on the network list. Supported options are append (default) or overwrite.
+- `--network`: activation network: production or staging.
+- `--email`: comma-delimited email list for activation email notifications.
+- `--comment`: activation comments.
+- `--config`: full or relative path to .edgerc file. Default is ~/.edgerc.
+- `--section`: the section of the .edgerc file with the proper {OPEN} API credentials. Default is default.
+- `--accountkey`: pass an account switch key in a request to manage an account different from the one in which you created your client.
+
+## Authentication
+Authentication is handled through Akamai EdgeGrid in the _provider.tf_ file.
+Ensure you have your _.edgerc_ file available and that it contains your Akamai EdgeGrid tokens separated in sections.
+
+```bash
+[default]
+client_secret = xxxx
+host = xxxx # unique string followed by `luna.akamaiapis.net`
+access_token = xxxx
+client_token = xxxx
+max-body = xxxx
+#account_key = xxxxx # specify the Account Switch Key to handle another another account with your Akamai Internal Credentials
+```
+
+For more information on Akamai EdgeGrid, _.edgerc_ file and creating Akamai EdgeGrid tokens, see ['Get started with APIs'](https://techdocs.akamai.com/developer/docs/set-up-authentication-credentials).
+
+# Contributing
+
+We're happy to accept help from fellow code-monkeys.
+Report issues/questions/feature requests on in the [issues] section.
+Refer to the [contribution guidelines](./contributing.md) for information on contributing to this module.
+
+# Authors
+
+- [@Benjamin Brouard](https://www.github.com/brrbrr), Security Professional Services @Akamai.
+
+# License
+
+[Apache License 2](https://choosealicense.com/licenses/apache-2.0/). See [LICENSE](./LICENSE.md) for full details.
---
+++ network-list-toolkit.py
@@ -0,0 +1,183 @@
+import logging
+import argparse
+import os
+import sys
+import requests
+import urllib.request
+import csv
+import ipaddress
+from time import sleep
+from akamai.edgegrid import EdgeGridAuth, EdgeRc
+
+# Setup logging
+logging.basicConfig(level='INFO', format='%(asctime)s %(levelname)s %(message)s')
+log = logging.getLogger()
+
+# Argparse will help manage command line arguments
+parser = argparse.ArgumentParser(description='Custom-Built Managed Network List Toolkit',
+                                 epilog='Built for automation of network list - bbrouard 2023',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+# Let's add a positional argument
+parser.add_argument('name', metavar='list-name', type=str, help='The name of our network list.')
+
+# Let's add optional arguments
+parser.add_argument('--file', action="store", default=os.getcwd() + "/list.csv", help="Path to CSV file with IPs for the network list.")
+parser.add_argument('--delimiter', action="store", default=',', help="CSV delimiter used. Default=\',\'")
+parser.add_argument('--url', action="store", help="URL to CSV file with IPs for the network list.")
+parser.add_argument('--action', action="store", default='append', help="The action to take on the network list. Supported options are: append or overwrite.")
+parser.add_argument('--network', action="store", help="Activation network: production or staging")
+parser.add_argument('--email', action="store", default='noreply@example.com', help="Comma delimited e-mail list, for activation e-mail notifications.")
+parser.add_argument('--comment', action="store", default="None.", help="Activation comments.")
+
+# Let's add edgerc options for flexibility
+parser.add_argument('--config', action="store", default=os.environ['HOME'] + "/.edgerc", help="Full or relative path to .edgerc file")
+parser.add_argument('--section', action="store", default="default", help="The section of the edgerc file with the proper {OPEN} API credentials.")
+parser.add_argument('--accountkey', action="store", help="Pass an account switch key in a request to manage an account different from the one in which you created your client.")
+
+args = parser.parse_args()
+
+""" 
+##########################################
+Edgegrid auth
+##########################################
+"""
+try:
+    edgerc = EdgeRc(args.config)
+    baseurl = 'https://%s' % edgerc.get(args.section, 'host')
+    if args.accountkey:
+        ask = args.accountkey
+    else: 
+        try:
+            ask = '%s' % edgerc.get(args.section, 'account_key')  
+        except Exception:
+            ask = ''
+    session = requests.Session()
+    session.auth = EdgeGridAuth.from_edgerc(edgerc, args.section)
+    log.debug('API Base URL: ' + baseurl)
+
+except Exception as e:
+    log.error('Error authenticating Akamai {OPEN} API client.')
+    log.error(e)
+
+""" 
+##########################################
+START OF THE SRIPT
+##########################################
+"""
+log.info('Updating network list \'' + args.name + '\'. Action: ' + args.action)
+
+# Find our list, using the 'name' argument and obtain the Network List ID
+endpoint = baseurl + '/network-list/v2/network-lists?accountSwitchKey=' + ask + '&search=' + args.name
+result = session.get(endpoint).json()
+
+# Check that the list exists, and there is no ambiguity (more than 1 list)
+if len(result['networkLists']) != 1:
+    log.error('Network list \'' + args.name + '\' not found. Please verify the name. Exiting.')
+    parser.print_help()
+    sys.exit(1)
+
+# Initialize the listID as a variable
+listId = result['networkLists'][0]['uniqueId']
+
+# Get the list details
+endpoint = baseurl + '/network-list/v2/network-lists/' + listId + '?accountSwitchKey=' + ask
+result = session.get(endpoint).json()
+
+
+# Fetch the list based on the supplied action (url or file)
+if args.url:
+    #Dowload list of IP from the URL provided and placing into list.csv file
+    urllib.request.urlretrieve(args.url, args.file)  
+    log.info('Downloading CSV file at URL: ' + args.url)
+
+# Ensure that the file exists
+if not os.path.isfile(args.file):
+    log.error('No input file was found or provided. Exiting.')
+    parser.print_help()
+    sys.exit(1)
+
+# Open the file
+try:
+    with open(args.file, 'r') as csvfile:
+        # Read in file contents as a list
+        csvreader = csv.reader(csvfile, delimiter=args.delimiter, dialect="excel")
+        log.info('Reading input file: ' + args.file + '. Size (bytes): ' + str(os.stat(args.file).st_size))
+        # We'll use this list to hold our IPs
+        sanitizedIps = []
+        # Open the CSV file and read each row
+        for row in csvreader:
+            # Loop over each column in the row
+            for value in row:
+                try:
+                    ipaddress.ip_address(value)
+                    sanitizedIps.append(value)
+                except ValueError:
+                    continue
+        log.info('Found ' + str(len(sanitizedIps)) + ' IP addresses in the list. Network List \'' + args.name + '\' previously had ' + str(result['elementCount']) + ' items.')       
+except Exception as e:
+    log.error('Error encountered opening CSV file: ' + args.file)
+    log.error(e)
+
+
+# Generating payload using python Dict operations
+log.info('Generating JSON payload for Network Lists API...')
+result['list'] = sanitizedIps
+
+log.info('Updating network list: ' + args.name)
+endpoint = baseurl + '/network-list/v2/network-lists/' + listId
+
+# Update the list based on the supplied action (overwrite or append)
+if args.action == 'append':
+    endpoint = endpoint  + '/append' + '?accountSwitchKey=' + ask
+    # Append requires simplified payload
+    result = {"list": sanitizedIps}
+    # Append requires POST method
+    method = 'POST'
+else:
+    # Overwrite (update) requires PUT method
+    endpoint = endpoint  + '?accountSwitchKey=' + ask
+    method = 'PUT'
+
+result = session.request(method, endpoint, json=result, headers={'Content-Type': 'application/json'})
+
+# check status code and log result if not 202
+if result.status_code != 202:
+    log.error('Update failed. Status Code: ' + str(result.status_code) + '. Result: ' + result.text)
+    sys.exit(1)
+else:
+    log.info('Update complete. Status Code: ' + str(result.status_code))
+
+    ################################### ACTIVATION
+    if args.network:
+        log.info('Activating network list \'' + args.name + '\' on ' + args.network.upper() + ' network.')
+
+        def checkStatus(listId, network):
+            endpoint = baseurl + '/network-list/v2/network-lists/' + listId + '/environments/' + network.upper() + '/status' + '?accountSwitchKey=' + ask
+            result = session.get(endpoint).json()
+            return result
+
+        result = checkStatus(listId, args.network)
+        if result['activationStatus'] == 'ACTIVE':
+            log.error('Network List \'' + args.name + '\' is already active. Exiting.')
+            log.info(result)
+            sys.exit(1)
+
+        log.info('Creating activation payload.')
+        emailList = args.email.split(",")
+        endpoint = baseurl + '/network-list/v2/network-lists/' + listId + '/environments/' + args.network.upper() + '/activate' + '?accountSwitchKey=' + ask
+        payload = { "comments": args.comment, "notificationRecipients": emailList}
+        result = session.post(endpoint, json=payload, headers={'Content-Type': 'application/json'}).json()
+
+        if not result['activationId']:
+            log.error('Error requesting activation. Exiting.')
+            sys.exit(1)
+
+        log.info('Activation request on network: ' + args.network + ' complete. Activation Id: ' + str(result['activationId']))
+        result = checkStatus(listId, args.network)
+
+        while result['activationStatus'] != 'ACTIVE':
+            log.info('Activation status on ' + args.network.upper() + ': ' + result['activationStatus'])
+            result = checkStatus(listId, args.network)
+            sleep(10)
+
+        log.info('Activation complete in network: ' + args.network + '. Status: ' + result['activationStatus'])
